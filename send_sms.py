5e1ca6ac94be86859468c60260540a50d07114c4
ShukujiNeel13 <neelshukuji@gmail.com>
2021-03-02 11:04:08 +0800

    Fetch the message record after it is created.

--- send_sms.py
+++ send_sms.py
@@ -1,3 +1,4 @@
+from pprint import pformat
 from twilio.rest import Client
 
 
@@ -19,10 +20,17 @@ SMS_TARGET_PHONE_NUMBER = '+xx00000000'
 ARGS_FOR_SEND_SMS = {'to': SMS_TARGET_PHONE_NUMBER, 'from_': TWILIO_ACCOUNT_SENDER_PHONE_NUMBER, 'body': 'Bless you World!'}
 
 
+# Create a Message Record
 MESSAGE = TWILIO_CLIENT.messages.create(**ARGS_FOR_SEND_SMS)
 
 
+# Get Message SID after creating Message Record
 MESSAGE_SID = MESSAGE.sid
 
-
 print(f'Created Twilio Message. SID is: {MESSAGE_SID}') 
+
+
+# Get an existing Message Record
+EXISTING_MESSAGE_RECORD = TWILIO_CLIENT.messages.get(MESSAGE_SID)
+print(f'Record for Message ID: {MESSAGE_SID} fetched as:)
+print(pformat(EXISTING_MESSAGE_RECORD))
